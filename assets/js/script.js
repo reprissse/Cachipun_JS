712c1013b8fcd3c49e96eb7c596cc9e82a07b9f3
Raquel Jimenez <raquel.jimenez.ramirez@gmail.com>
2024-02-10 22:34:13 -0300

    :sparkles: se ingresa las reglas del juego

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// Función que determina el ganador del juego
+function logicaJuego(decisionUsuario, decisionComputador) {
+    if (decisionUsuario === decisionComputador) {
+        console.log("EMPATE, ambos eligieron " + decisionUsuario);
+    } else if (
+        (decisionUsuario === "piedra" && decisionComputador === "papel") ||
+        (decisionUsuario === "papel" && decisionComputador === "tijera") ||
+        (decisionUsuario === "tijera" && decisionComputador === "piedra")
+    ) {
+        console.log(decisionUsuario + " vs " + decisionComputador + " GANA el computador");
+    } else {
+        console.log(decisionUsuario + " vs " + decisionComputador + " GANA el usuario");
+    }
+}
